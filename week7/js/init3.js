cf2fb4286e50a0611a02a7b4508e2096c33502ce
haven <ahjovel@gmail.com>
2022-05-17 23:53:00 -0700

    done

--- week7/js/init3.js
+++ week7/js/init3.js
@@ -13,8 +13,8 @@ let layers = {
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
 
 let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+    maxZoom: 16
 });
 
 Esri_WorldGrayCanvas.addTo(map)
